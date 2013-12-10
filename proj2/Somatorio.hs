4abb1e75cd223fda1c83ff69c251b09309ac280e
tonussi <lptonussi@gmail.com>
2013-12-10 00:59:53 -0200

    Retorna apenas a somaLista (mais simples)

--- proj2/Somatorio.hs
+++ proj2/Somatorio.hs
@@ -1,27 +1,28 @@
 module Somatorio
-( somaInteiros
-, numImpares
-, numPares
-, quadInteiros
-, quadImpares
+( somaListaInteiros
+, somaListaImpares
+, somaListaPares
+, somaListaQuadInteiros
+, somaListaQuadImpares
 ) where
 
-
 somaInteiros :: Float -> Float
-somaInteiros n = map (\x -> (x * (x + 1)/2)) [1..n])
+somaInteiros n = map (\x -> (x * (x + 1)/2)) [1..n]
 
 somaListaInteiros :: :: Float -> Float
-somaListaInteiros n = last somaInt n
+somaListaInteiros n = last (somaInteiros n)
 
 numImpares :: Float -> Float
-numImpares n = map (\x -> (x * (x + 1)/2)) [1..n])
+numImpares n = map (\x -> (x * (x + 1)/2)) [1..n]
+
+somaListaImpares :: :: Float -> Float
+somaListaImpares n = last (numImpares n)
 
 numPares :: Float -> Float
-numPares n = map (\x -> (x * (x + 1)/2)) [1..n])
+numPares n = map (\x -> (x * (x + 1)/2)) [1..n]
 
 quadInteiros :: Float -> Float
-quadInteiros n = map (\x -> (x * (x + 1)/2)) [1..n])
+quadInteiros n = map (\x -> (x * (x + 1)/2)) [1..n]
 
 quadImpares :: Float -> Float
-quadImpares n = map (\x -> (x * (x + 1)/2)) [1..n])
-
+quadImpares n = map (\x -> (x * (x + 1)/2)) [1..n]
