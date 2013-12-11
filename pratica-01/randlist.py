d3188c550c486c4219cf0cc0e38f6696f8b340af
tonussi <lptonussi@gmail.com>
2013-12-11 11:12:38 -0200

    Add classe RandList.py para gerar um deck embaralhado

---
+++ pratica-01/randlist.py
@@ -0,0 +1,31 @@
+# encoding:utf-8
+
+import random
+
+def main():
+    rl = RandList(500, 1000)
+    rl.gen_random_list()
+    rl.print_block_list()
+    print "\n\nPronto!"
+
+class RandList(object):
+
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+        self.populacao = []
+
+    def gen_random_list(self):
+        self.populacao = range(self.first, self.second)
+        del self.populacao[28]
+        random.shuffle(self.populacao)
+
+    def print_block_list(self):
+        print "[",
+        for i in range(15):
+            for j in range(15):
+                print "%3d," % self.populacao[i*7+j],
+        print "]"
+
+if __name__ == "__main__":
+    main()
