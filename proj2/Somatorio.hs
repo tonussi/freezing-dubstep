02e6197eacc13dd22d1ce68963746530bf19eaa3
tonussi <lptonussi@gmail.com>
2013-12-11 14:42:04 -0200

    Coloca algumas funcoes invisiveis

--- proj2/Somatorio.hs
+++ proj2/Somatorio.hs
@@ -4,11 +4,11 @@ module Somatorio
 , somaListaPares -- problema
 , somaListaQuadInteiros
 , somaListaQuadImpares -- problema
-, numImpares
-, numPares
-, quadInteiros
-, quadImpares
-, fatorial
+, quaseUm
+, quaseDois
+, quaseE
+, quaseE2
+, quasePi
 ) where
 
 
@@ -31,9 +31,8 @@ somaListaInteiros n = last (somaInteiros n)
 numImpares :: (Enum b, Fractional b) => b -> [b]
 numImpares n = map (\x -> (x * (x + 2)/2)) [1..n]
 
-
--- para o haskell zero -> par
-
+-- n = 2k even
+-- n = 2k + 1 odd
 
 somaListaImpares :: (Fractional a, Integral a) => a -> a
 somaListaImpares n = if odd n
@@ -43,6 +42,7 @@ somaListaImpares n = if odd n
                      else somaListaImpares (n - 1)
 
 
+
 numPares :: (Enum b, Fractional b) => b -> [b]
 numPares n = map (\x -> (x * (x + 2)/2)) [2..n]
 
@@ -50,10 +50,11 @@ numPares n = map (\x -> (x * (x + 2)/2)) [2..n]
 
 somaListaPares :: (Fractional a, Integral a) => a -> a
 somaListaPares n = if even n
-                     then last (numPares n)
-                     else if n <= 0
-                     then 0.0
-                     else somaListaPares (n - 1)
+                   then last (numPares n)
+                   else if n <= 0
+                   then 0.0
+                   else somaListaPares (n - 1)
+
 
 
 quadInteiros :: (Enum b, Fractional b) => b -> [b]
@@ -63,25 +64,25 @@ quadInteiros n = map (\x -> (x * (x + 1) * (2 * x + 1) / 6)) [1..n]
 
 
 somaListaQuadInteiros :: (Enum a, Fractional a) => a -> a
-somaListaQuadInteiros n = last (quadInteiros n)
+somaListaQuadInteiros n = last (quadInteiros n) -- 1 + 4 + 9 + 16 + 25 = 55.0
+
+{- import Data.List
+   (odds,evens) = partition odd [1..]
+   take 10 odds -- [1,3,5,7,9,11,13,15,17,19]
+   take 10 evens -- [2,4,6,8,10,12,14,16,18,20]
+-}
+
+-- quadImpares :: (Enum b, Num b) => b -> [b]
+quadImpares n = map (\x -> (x * (x + 1) * (2 * x + 1) / 6)) [1..n]
 
 
 
-quadImpares :: (Enum b, Num b) => b -> [b]
-quadImpares n = map (\x -> (x^2)) [1..(2*n-1)]
+-- somaListaQuadImpares :: (Fractional a, Integral a) => a -> a
+somaListaQuadImpares n = (somaListaQuadInteiros n)
 
 
 
-somaListaQuadImpares :: (Fractional a, Integral a) => a -> a
-somaListaQuadImpares n = if odd n
-                         then last (quadImpares n)
-                         else if n <= 0
-                         then 0.0
-                         else somaListaQuadImpares (n - 1)
-
-
-
-{- Series Infinitas
+{- Series Infinitas em Haskell
 -}
 
 
@@ -89,17 +90,87 @@ somaListaQuadImpares n = if odd n
 quaseUm :: (Enum a, Fractional a) => Int -> a
 quaseUm n = sum (map (\x -> (1/(x^2+x))) (take n [1..])) -- 0.9900990099009898
 
-{- quase dois por baixo
-   1.99 < 2 mas poderia
-   ser feito 2.01 < 2
-   que seria quase dois
-   porem por cima.
+
+
+quaseDois :: (Enum a, Fractional a) => Int -> a
+quaseDois n = sum (map (\x -> (2/(x^2+x))) (take n [1..]))
+
+
+
+{- Pointless (hahamm) “Points-free” Factorial
 -}
-quaseDois n = sum (map (\x -> (2/(x^2+x))) (take n [1..])) -- 1.9999950030606968
 
 
-fatorial n = if n > 0 then n * fatorial (n-1) else 1
 
--- calculo euler number (1 + (1/n)) ^ n
+fatorialPfree :: Double -> Double
+fatorialPfree = foldl (*) 1 . enumFromTo 1
 
-quaseE n = sum (map (\x ->  ((1 + 1/x)^x)   ) (take n [1..])) -- 2.71828182845904523536028747135266249775724709369995
+
+
+{- Usando product
+-}
+
+
+
+fatorialProdutorio :: (Enum a, Num a) => a -> a
+fatorialProdutorio n = product [1..n]
+
+
+
+quaseE :: Int -> Double
+quaseE n = 1 + sum (map (\x ->  1/(fatorialPfree x) ) (take n [1..]))
+
+
+
+{- Prelude> quaseE 10 (fast)
+   2.718281801146385
+   Prelude> quaseE 100 (fast)
+   2.7182818284590455
+   Prelude> quaseE 1000 (fast)
+   2.7182818284590455
+   Prelude> quaseE 10000 (slow - going crash)
+   2.7182818284590455
+
+
+   Tudo por que isso na recurss.
+   comeca a ficar muito custoso
+
+   call fact()
+       call fact()
+           call fact()
+           ...
+           cleanup
+       cleanup
+   cleanup
+-}
+
+
+
+quaseE2 :: (Enum a, Fractional a, Integral a1) => a1 -> a
+quaseE2 n = 1 + sum (map parcelasNumeroE [1..n])
+
+
+
+{- Prelude> quaseE2 10 (fast)
+   2.718281801146385
+   Prelude> quaseE2 100 (fast)
+   2.7182818284590455
+   Prelude> quaseE2 1000 (fast)
+   2.7182818284590455
+   Prelude> quaseE2 10000 (slow - going crash)
+   2.7182818284590455
+-}
+
+
+parcelasNumeroE :: (Enum a1, Fractional a1, Integral a) => a -> a1
+parcelasNumeroE x = 1/(fatorialProdutorio k) where k = fromIntegral x
+
+
+
+quasePi :: Int -> Double
+quasePi n = sum (map parcelasPi [1..n])
+
+
+
+parcelasPi :: Int -> Double
+parcelasPi x = 4*(-1)^(x+1) / (2.0 * k - 1) where k = fromIntegral x -- 3.1416026536897204
