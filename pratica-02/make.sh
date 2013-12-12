679311dd10436dbed1297997a34c33d935e280ea
tonussi <lptonussi@gmail.com>
2013-12-12 14:58:29 -0200

    Adiciona frases de aviso ao usuario

--- pratica-02/make.sh
+++ pratica-02/make.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo "Compilando, espera.."
 gcc -O2 -Wall -o bytes     bytes.c
 gcc -O2 -Wall -o double_pi double_pi.c
 gcc -O2 -Wall -o int_float int_float.c
 gcc -O2 -Wall -o tipos     tipos.c
+echo "Finalizado."
