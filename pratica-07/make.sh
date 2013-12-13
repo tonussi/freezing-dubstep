63fce33ba8906cf9280f2aaf4e5056219647b454
tonussi <lptonussi@gmail.com>
2013-12-13 20:50:34 -0200

    Add makesh melhorado (mais explicativo)

--- pratica-07/make.sh
+++ pratica-07/make.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
-# echo "Compilando, espera.."
-# gcc -O2 -Wall -ansi -o stret sol_triang.c soluc.c -lm
-# echo "Finalizado."
+echo "gcc -O2 -Wall -ansi -o stret sol_triang.c soluc.c -lm"
+gcc -O2 -Wall -ansi -o stret sol_triang.c soluc.c -lm
+echo "./stret"
+./stret
+echo "rm stret"
+rm stret
