a34c48a818134ac58f858ce9093de5269b8fb61e
tonussi <lptonussi@gmail.com>
2013-12-14 19:07:10 -0200

    Add testes modulo Listas.hs lab10

--- pratica-10/Listas.hs
+++ pratica-10/Listas.hs
@@ -1,15 +1,28 @@
 module Listas
 ( unittest
 , primeiros
+, calculaSomaPA
+, calculaProdPA
+, mapeiaListaPA
 ) where
-
-
 
 primeiros 0 _ = []
 primeiros n [] = []
 primeiros n (a:x) = a : primeiros (n - 1) x
 
+fact = foldl (*) 1 . enumFromTo 1
 
+-- a0 = 0
+
+calculaSomaPA n razao = sum (map (\x -> razao + ((x - 1) * razao)) (take n [0..]))
+
+-- a0 = 0
+
+mapeiaListaPA n razao = map (\x -> razao + ((x - 1) * razao)) (take n [0..])
+
+-- a0 = 1
+
+calculaProdPA n razao = product (map (\x -> razao + ((x - 1) * razao)) (take n [1..]))
 
 unittest = do
   putStrLn "Haskel::MiniTest"
@@ -24,3 +37,21 @@ unittest = do
   print (primeiros  6 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
   print (primeiros  7 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
   print (primeiros  8 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
+
+  putStrLn "Test Mapeamentos Somas Produtos"
+
+  print (mapeiaListaPA 7 7)
+  print (calculaSomaPA 7 7)
+  print (calculaProdPA 7 7)
+
+  print (mapeiaListaPA 4 7)
+  print (calculaSomaPA 4 7)
+  print (calculaProdPA 4 7)
+
+  print (mapeiaListaPA 3 6)
+  print (calculaSomaPA 3 6)
+  print (calculaProdPA 3 6)
+
+  print (mapeiaListaPA 23 12)
+  print (calculaSomaPA 23 12)
+  print (calculaProdPA 23 12)
