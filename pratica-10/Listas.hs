21dfd0c0128b0d55f7561028de6ad4d72981ddc0
tonussi <lptonussi@gmail.com>
2013-12-14 11:12:33 -0200

    Add funcao primeiros ela obtem os n primeiros termos de uma lista

--- pratica-10/Listas.hs
+++ pratica-10/Listas.hs
@@ -1,7 +1,27 @@
 module Listas
 ( unittest
-,
+, primeiros
 ) where
+
+
+
+primeiros 0 _ = []
+primeiros _ [] = []
+primeiros n (a:x) = a : primeiros (n - 1) x
+
+
 
 unittest = do
   putStrLn "Haskel::MiniTest"
+
+  putStrLn "Test Primeiros"
+  print (primeiros [])
+  print (primeiros 0)
+  print (primeiros 1)
+  print (primeiros 2)
+  print (primeiros 3)
+  print (primeiros 4)
+  print (primeiros 5)
+  print (primeiros 6)
+  print (primeiros 7)
+  print (primeiros 8)
