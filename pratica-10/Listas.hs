37d174e7d4ce0f2260a8de8d44c5fa9c6732e024
tonussi <lptonussi@gmail.com>
2013-12-14 11:17:50 -0200

    TTDreport: primeiros passou nos teste

--- pratica-10/Listas.hs
+++ pratica-10/Listas.hs
@@ -6,7 +6,7 @@ module Listas
 
 
 primeiros 0 _ = []
-primeiros _ [] = []
+primeiros n [] = []
 primeiros n (a:x) = a : primeiros (n - 1) x
 
 
@@ -15,13 +15,12 @@ unittest = do
   putStrLn "Haskel::MiniTest"
 
   putStrLn "Test Primeiros"
-  print (primeiros [])
-  print (primeiros 0)
-  print (primeiros 1)
-  print (primeiros 2)
-  print (primeiros 3)
-  print (primeiros 4)
-  print (primeiros 5)
-  print (primeiros 6)
-  print (primeiros 7)
-  print (primeiros 8)
+  print (primeiros  0 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
+  print (primeiros  1 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
+  print (primeiros  2 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
+  print (primeiros  3 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
+  print (primeiros  4 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
+  print (primeiros  5 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
+  print (primeiros  6 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
+  print (primeiros  7 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
+  print (primeiros  8 [1897, 1014, 783, 952,  67, 1848, 468, 892, 833, 1439, 365, 115, 335])
